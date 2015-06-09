e496d85878758e5540f48a471daa503a391402d6
Fyodor Patlin <patlin_fv@miac-tmn.ru>
2015-06-09 15:08:55 +0500

    Fix imports, format

--- main.go
+++ main.go
@@ -2,37 +2,38 @@
 package main
 
 import (
-	"fmt"
 	"flag"
+	"fmt"
 	"log"
 	"os"
-	"github.com/fedorpatlin/sapccms"
+
+	"github.com/fedorpatlin/sapccmsget/sapccms"
 )
 
 var flags struct {
 	mtename *string
-	host	*string
-	sapsid	*string
-	instNr	*string
-//	mode    *string // avg1, avg5, avg15
+	host    *string
+	sapsid  *string
+	instNr  *string
+	//	mode    *string // avg1, avg5, avg15
 }
 
-func prepareUsage(){
-	hostname,_ := os.Hostname()
+func prepareUsage() {
+	hostname, _ := os.Hostname()
 	flags.sapsid = flag.String("s", "", "SAP system name")
 	flags.host = flag.String("h", hostname, "SAP host name")
 	flags.instNr = flag.String("n", "00", "Instance number")
-//	flags.mode = flag.String("m", "avg1", "Value type of avg1, avg5, avg15" )
+	//	flags.mode = flag.String("m", "avg1", "Value type of avg1, avg5, avg15" )
 	flags.mtename = flag.String("e", "", "CCMS monitoring tree element full name")
 }
 
-func usage(){
-	
+func usage() {
+
 }
 
-func createMteRequest() *sapccms.MsgMtGetTidByNameRequest{
+func createMteRequest() *sapccms.MsgMtGetTidByNameRequest {
 	req := sapccms.NewMsgMtGetTidByNameRequest()
-	req.SoapRequest.Item=make([]sapccms.ALMTNAMEL,1)
+	req.SoapRequest.Item = make([]sapccms.ALMTNAMEL, 1)
 	if flags.mtename == nil {
 		flag.PrintDefaults()
 		os.Exit(-1)
@@ -41,34 +42,36 @@ func createMteRequest() *sapccms.MsgMtGetTidByNameRequest{
 	return req
 }
 
-func createMteResponse() *sapccms.MsgMtGetTidByNameResponse{
+func createMteResponse() *sapccms.MsgMtGetTidByNameResponse {
 	res := sapccms.NewMsgMtGetTidByNameResponse()
 	return res
 }
 
-func getMteByName(srv sapccms.SAPCCMS, name string)*sapccms.ALGTIDLNRC{
-		var mteRequest = createMteRequest()
+func getMteByName(srv sapccms.SAPCCMS, name string) *sapccms.ALGTIDLNRC {
+	var mteRequest = createMteRequest()
 	var mteResponse = createMteResponse()
-	
+
 	if err := srv.MtGetTidByName(mteRequest, mteResponse); err != nil {
 		log.Fatal(err.Error())
 	}
-	if mteResponse.GetTidTable().GetItem()[0].Rc=="0"{
+	if mteResponse.GetTidTable().GetItem()[0].Rc == "0" {
 		return &mteResponse.GetTidTable().GetItem()[0]
-	} else {return nil}
+	} else {
+		return nil
+	}
 }
 
-func preparePerfRequest(tid sapccms.ALGLOBTID) *sapccms.MsgPerfReadRequest{
+func preparePerfRequest(tid sapccms.ALGLOBTID) *sapccms.MsgPerfReadRequest {
 	msg := sapccms.NewMsgPerfReadRequest()
-	msg.SoapRequest.Item = make([]sapccms.ALGLOBTID,1)
+	msg.SoapRequest.Item = make([]sapccms.ALGLOBTID, 1)
 	msg.SoapRequest.Item[0] = tid
 	return msg
 }
 
-func getPerfByTid(srv sapccms.SAPCCMS, tid sapccms.ALGLOBTID) *sapccms.MsgPerfReadResponse{
+func getPerfByTid(srv sapccms.SAPCCMS, tid sapccms.ALGLOBTID) *sapccms.MsgPerfReadResponse {
 	sendMsg := preparePerfRequest(tid)
 	rcvMsg := sapccms.NewMsgPerfReadResponse()
-	if err := srv.PerfRead(sendMsg, rcvMsg);err != nil{
+	if err := srv.PerfRead(sendMsg, rcvMsg); err != nil {
 		log.Fatal(err.Error())
 	}
 	return rcvMsg
@@ -79,14 +82,14 @@ func main() {
 	flag.Parse()
 	srv := sapccms.NewSAPCCMS(fmt.Sprintf("http://%s:5%s13/SAPCCMS.cgi", *flags.host, *flags.instNr))
 	tid := getMteByName(srv, *flags.mtename)
-	if tid == nil{
-		log.Fatalln("Element not found by name "+*flags.mtename)
+	if tid == nil {
+		log.Fatalln("Element not found by name " + *flags.mtename)
 	}
 	perf := getPerfByTid(srv, tid.Tid)
 	perfValue := perf.PerfReadResponse.TidTable.Item[0].PerfValue
-	if perfValue.Avg01CountValue == "0" && perfValue.Avg01CountValue == "0"{
+	if perfValue.Avg01CountValue == "0" && perfValue.Avg01CountValue == "0" {
 		fmt.Println(perfValue.AlertRelevantValue)
-	}else{
+	} else {
 		fmt.Println(perfValue.Avg01PerfValue)
 	}
 }
